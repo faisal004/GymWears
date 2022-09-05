9c1c76ef92a892fa66ece4b8bf435d13b12392a0
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2022-09-05 10:19:17 +0530

    Monday

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,21 +2,19 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRef } from "react";
-import { AiOutlineShoppingCart ,AiFillCloseCircle} from "react-icons/Ai";
+import { AiOutlineShoppingCart, AiFillCloseCircle } from "react-icons/Ai";
 
 const Navbar = () => {
-  const togglecart = () =>{
-    if(ref.current.classList.contains("translate-x-full")){
-      ref.current.classList.remove("translate-x-full")
-      ref.current.classList.add("translate-x-0")
+  const togglecart = () => {
+    if (ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.remove("translate-x-full");
+      ref.current.classList.add("translate-x-0");
+    } else if (!ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.remove("translate-x-0");
+      ref.current.classList.add("translate-x-full");
     }
-    else if(!ref.current.classList.contains("translate-x-full")){
-      ref.current.classList.remove("translate-x-0")
-      ref.current.classList.add("translate-x-full")
-    }
-
-  }
-  const ref = useRef()
+  };
+  const ref = useRef();
   return (
     <div className="flex flex-col md:flex-row md:justify-start justify-center items-center shadow-xl ">
       <div className="logo">
@@ -46,17 +44,32 @@ const Navbar = () => {
           </Link>
         </ul>
       </div>
-      <div onClick={togglecart} className="cart absolute right-0 px-2 py-2 text-3xl  md:text-6xl cursor-pointer">
+      <div
+        onClick={togglecart}
+        className="cart absolute right-0 px-2 py-2 text-3xl  md:text-6xl cursor-pointer"
+      >
         <AiOutlineShoppingCart />
       </div>
-      <div ref={ref} className=" sidecart absolute top-0 right-0 bg-slate-300 p-10 text-xl font-semibold transform transition-transform translate-x-full">
+      <div
+        ref={ref}
+        className="sidecart absolute top-0 right-0 bg-slate-300 p-10 text-xl font-semibold transform transition-transform translate-x-full"
+      >
         <h2 className="flex flex-row">
           ShoppingCart
           <span>
             <AiOutlineShoppingCart className="m-1" />
           </span>
         </h2>
-        <span onClick={togglecart} className="absolute top-2 right-2"><AiFillCloseCircle className="cursor-pointer text-slate-700"/></span>
+        <span onClick={togglecart} className="absolute top-2 right-2">
+          <AiFillCloseCircle className="cursor-pointer text-slate-700" />
+        </span>
+        <ol>
+          <li>fdfd</li>
+          <li>fdfd</li>
+          <li>fdfd</li>
+          <li>fdfd</li>
+          <li>fdfd</li>
+        </ol>
       </div>
     </div>
   );
