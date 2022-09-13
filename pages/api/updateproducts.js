d1c43299adfeda8a59554135f1a4ee70af1b1d5e
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2022-09-13 11:02:04 +0530

    tuesday

---
+++ pages/api/updateproducts.js
@@ -0,0 +1,16 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import connectDB from "../../middleware/mongoose";
+import Product from "../../models/product";
+const handler = async (req, res) => {
+  if (req.method == 'POST') {
+    for (let i = 0; i < req.body.length; i++) {
+      let p = await Product.findByIdAndUpdate(req.body[i]._id,req.body[i])
+     
+    }
+    res.status(200).json({ succes: "succes" });
+  } else {
+    res.status(400).json({ error: "This method is not allowed" });
+  }
+};
+export default connectDB(handler);
