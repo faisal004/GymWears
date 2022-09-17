a6954263bfd9dc7184688bd8f705671843a576c7
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2022-09-17 13:34:57 +0530

    Saturday

--- pages/api/getProduct.js
+++ pages/api/getProduct.js
@@ -1,33 +1,35 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import connectDB from '../../middleware/mongoose'
-import Product from '../../models/product'
-const handler=async(req,res)=>{
-
-    let products=await Product.find()
-    let tshirts={}
-    for(let item of products){
-      if(item.title in tshirts){
-
-      }else{
-        tshirts[item.title]=JSON.parse(JSON.stringify(item))
-        if(item.availableQty){
-          tshirts[item.title].color=[item.color]
-          tshirts[item.title].size=[item.size]
-
-
-        }
+import connectDB from "../../middleware/mongoose";
+import Product from "../../models/product";
+const handler = async (req, res) => {
+  let products = await Product.find();
+  let tshirts = {};
+  for (let item of products) {
+    if (item.title in tshirts) {
+      if (
+        !tshirts[item.title].color.includes(item.color) &&
+        item.availableQty > 0
+      ) {
+        tshirts[item.title].color.push(item.color);
       }
-
-
+      if (
+        !tshirts[item.title].size.includes(item.size) &&
+        item.availableQty > 0
+      ) {
+        tshirts[item.title].size.push(item.size);
+      }
+    } else {
+      tshirts[item.title] = JSON.parse(JSON.stringify(item));
+      if (item.availableQty) {
+        tshirts[item.title].color = [item.color];
+        tshirts[item.title].size = [item.size];
+      }
     }
-
-
-
-
-    res.status(200).json({ products })
   }
-  export default connectDB(handler);
 
-  //this is a comment
-  
+  res.status(200).json({ tshirts });
+};
+export default connectDB(handler);
+
+//this is a comment
--- pages/tshirt.js
+++ pages/tshirt.js
@@ -3,33 +3,72 @@ import React from "react";
 import mongoose from "mongoose";
 import Product from "../models/product";
 
-const tshirt = ({products}) => {
+const tshirt = ({ products }) => {
   return (
     <div>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
-          {products.map((item)=> {
-             return <Link passHref={true} key={item._id} href={`/product/${item.slug}`}>
-              <div className="m-auto lg:w-1/4 md:w-1/2 p-4  shadow-xl cursor-pointer border-black">
-                <a className="block rounded overflow-hidden">
-                  <img
-                    alt="ecommerce"
-                    className="object-cover object-top w-full h-96  block"
-                    src={item.img}
-                  />
-                </a>
-                <div className="mt-4">
-                  <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                    T-shirts
-                  </h3>
-                  <h2 className="text-gray-900 title-font text-lg font-medium">
-                    {item.title}
-                  </h2>
-                  <p className="mt-1">{item.price}</p>
-                </div>
-              </div>
-            </Link>})}
+            {Object.keys(products).map((item) => {
+              return (
+                <Link
+                  passHref={true}
+                  key={products[item]._id}
+                  href={`/product/${products[item].slug}`}
+                >
+                  <div className="m-auto lg:w-1/4 md:w-1/2 p-4  shadow-xl cursor-pointer border-black">
+                    <a className="block rounded overflow-hidden">
+                      <img
+                        alt="ecommerce"
+                        className="object-cover object-top w-full h-96  block"
+                        src={products[item].img}
+                      />
+                    </a>
+                    <div className="mt-4">
+                      <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                        T-shirts
+                      </h3>
+                      <h2 className="text-gray-900 title-font text-lg font-medium">
+                        {products[item].title}
+                      </h2>
+                      <p className="mt-1">{products[item].price}</p>
+                      <div className="mt-1">
+                        {products[item].size.includes("S") && (
+                          <span className="border border-gray-300 px-1 mx-1">
+                            S
+                          </span>
+                        )}
+                        {products[item].size.includes("M") && (
+                          <span className="border border-gray-300 px-1 mx-1">
+                            M
+                          </span>
+                        )}
+                        {products[item].size.includes("L") && (
+                          <span className="border border-gray-300 px-1 mx-1">
+                            L
+                          </span>
+                        )}
+                        {products[item].size.includes("XL") && (
+                          <span className="border border-gray-300 px-1 mx-1">
+                            XL
+                          </span>
+                        )}
+                        {products[item].size.includes("XXL") && (
+                          <span className="border border-gray-300 px-1 mx-1">
+                            XXL
+                          </span>
+                        )}
+                      </div>
+                      <div className="mt-1">
+                        {products[item].color .includes("red") && 
+                          <button className="border-2 border-gray-300 ml-1 bg-red-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                        }
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              );
+            })}
           </div>
         </div>
       </section>
@@ -41,12 +80,34 @@ export async function getServerSideProps(_context) {
   if (mongoose.connections[0].readyState) {
     await mongoose.connect(process.env.MONGO_URI);
   }
-  
-  
-  let products = await Product.find({catgory:'T-shirt'});
-  
+
+  let products = await Product.find({ catgory: "T-shirt" });
+  let tshirts = {};
+  for (let item of products) {
+    if (item.title in tshirts) {
+      if (
+        !tshirts[item.title].color.includes(item.color) &&
+        item.availableQty > 0
+      ) {
+        tshirts[item.title].color.push(item.color);
+      }
+      if (
+        !tshirts[item.title].size.includes(item.size) &&
+        item.availableQty > 0
+      ) {
+        tshirts[item.title].size.push(item.size);
+      }
+    } else {
+      tshirts[item.title] = JSON.parse(JSON.stringify(item));
+      if (item.availableQty) {
+        tshirts[item.title].color = [item.color];
+        tshirts[item.title].size = [item.size];
+      }
+    }
+  }
+
   return {
-    props: {products: JSON.parse(JSON.stringify(products))}, // will be passed to the page component as props
+    props: { products: JSON.parse(JSON.stringify(tshirts)) }, // will be passed to the page component as props
   };
 }
 
